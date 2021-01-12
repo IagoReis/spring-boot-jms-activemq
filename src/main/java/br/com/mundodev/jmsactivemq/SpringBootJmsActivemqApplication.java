c430bab7e470683d235202971e2cc6b773efa93a
Iago Reis <iagoidrs@gmail.com>
2021-01-12 19:57:11 -0300

    Ah shit, here we go again

---
+++ src/main/java/br/com/mundodev/jmsactivemq/SpringBootJmsActivemqApplication.java
@@ -0,0 +1,13 @@
+package br.com.mundodev.jmsactivemq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJmsActivemqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJmsActivemqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/mundodev/jmsactivemq/SpringBootJmsActivemqApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mundodev.jmsactivemq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJmsActivemqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
